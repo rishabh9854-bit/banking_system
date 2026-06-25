e321a9ed19382f4026d3fc28de459f04b6b24172
rishabh9101 <rishabh9854@gmail.com>
2026-06-25 10:37:03 +0530

    updated the main block

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@ int main()
 {
     int choice;
     bankacc acc;
-    
+    admin Admin;
+
     int accn;
-    
+
     streampos pos;
     do
     {
@@ -19,7 +20,8 @@ int main()
         cout << "4. Display Account\n";
         cout << "5. Display All\n";
         cout << "6. Delete account\n";
-        cout << "7. EXIT\n";
+        cout << "7. ADMIN PANEL\n";
+        cout << "8. EXIT\n";
         cout << "Enter choice: ";
         cin >> choice;
 
@@ -37,7 +39,7 @@ int main()
             cin >> accn;
 
             cout << "enter pin: " << endl;
-            
+
             if (acc.findaccount(accn, pos))
             {
                 if (acc.pinverifier())
@@ -61,39 +63,47 @@ int main()
             break;
 
         case 3:
-            cout<<"enter acc no:"<<endl;
-            cin>>accn;
-            if(acc.findaccount(accn,pos)){
-                if(acc.pinverifier()){
-                    cout<<"enter ammount to be withdrawn";
+            cout << "enter acc no:" << endl;
+            cin >> accn;
+            if (acc.findaccount(accn, pos))
+            {
+                if (acc.pinverifier())
+                {
+                    cout << "enter ammount to be withdrawn";
                     double amount;
-                    cin>>amount;
+                    cin >> amount;
                     acc.withdraw(amount);
                     acc.saveat(pos);
-                    cout<<"new bal: "<<acc.getbal()<<endl;
+                    cout << "new bal: " << acc.getbal() << endl;
                 }
-                else{
-                    cout<<"wrong pin"<<endl;
+                else
+                {
+                    cout << "wrong pin" << endl;
                 }
             }
-            else{
-                cout<<"ACC NOT FOUND"<<endl;
+            else
+            {
+                cout << "ACC NOT FOUND" << endl;
             }
             break;
 
         case 4:
-            cout<<"Enter acc no."<<endl;
-            cin>>accn;
-            if(acc.findaccount(accn,pos)){
-                if(acc.pinverifier()){
-                    cout<<acc.getbal()<<endl;
+            cout << "Enter acc no." << endl;
+            cin >> accn;
+            if (acc.findaccount(accn, pos))
+            {
+                if (acc.pinverifier())
+                {
+                    cout << acc.getbal() << endl;
                 }
-                else{
-                    cout<<"wrong pin"<<endl;
+                else
+                {
+                    cout << "wrong pin" << endl;
                 }
             }
-            else{
-                cout<<"wrong pin"<<endl;
+            else
+            {
+                cout << "wrong pin" << endl;
             }
             break;
 
@@ -101,20 +111,61 @@ int main()
             acc.displayall();
             break;
 
-        case(6):
-            cout<<"enter acc no. to delete"<<endl;
-            cin>>accn;
+        case (6):
+            cout << "enter acc no. to delete" << endl;
+            cin >> accn;
             acc.deleteaccount(accn);
             break;
-            
-        case(7):
+        case (7):
+            int adminchoice;
+            bool go;
+            if (!(Admin.isloggedin()))
+            {
+                go = Admin.login();
+            }
+            if (!(go))
+            {
+                break;
+            }
+            do
+            {
+                cout << "1. View All Accounts (with PINs)\n";
+                cout << "2. Delete Any Account\n";
+                cout << "3. Export All to CSV\n";
+                cout << "4. Logout & Return\n";
+                cout << "Enter choice: ";
+                cin >> adminchoice;
+                switch (adminchoice)
+                {
+                case (1):
+                    Admin.admin_display();
+                    break;
+                case (2):
+                    cout << "enter acc no. to del";
+                    int accnumber;
+                    cin >> accnumber;
+                    Admin.admindel(accnumber);
+                    break;
+                case (3):
+                    Admin.exporttocsv();
+                    break;
+                case (4):
+                    Admin.logout();
+                    break;
+                default:
+                    cout << "INVALID CHOICE!";
+                }
+            } while (adminchoice != 8);
+            break;
+
+        case (8):
             break;
 
         default:
             cout << "Invalid choice\n";
         }
 
-    } while (choice != 7);
+    } while (choice != 8);
 
     return 0;
 }
